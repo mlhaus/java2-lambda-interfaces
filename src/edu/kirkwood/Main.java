4b90516289f823d0529ab4f7b8dd73ef769aea27
Marc Hauschildt <marc.hauschildt@gmail.com>
2021-12-22 11:12:22 -0600

    Adding Main

---
+++ src/edu/kirkwood/Main.java
@@ -0,0 +1,11 @@
+package edu.kirkwood;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    ConsumerTest.main(args);
+//	    FunctionTest.main(args);
+//	    SupplierTest.main(args);
+	    BiPredicateTest.main(args);
+    }
+}
